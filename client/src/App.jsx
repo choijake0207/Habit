67e4da0ad259885646f7fa9aeb1c0e53b38ad852
choijake0207 <jake.ch0712@gmail.com>
2025-01-24 15:15:22 -0500

    set up basic CSR for pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router"
 import Root from './Layout/Root'
 import Dashboard from './Pages/Dashboard'
+import Analytics from "./Pages/Analytics"
+import Friends from "./Pages/Friends"
+import Profile from "./Pages/Profile"
+import Settings from "./Pages/Settings"
 
 
 
@@ -11,6 +15,10 @@ const router = createBrowserRouter(
     <>
       <Route path="/" element={<Root/>}>
         <Route index element={<Dashboard/>}/>
+        <Route path="/analytics" element={<Analytics/>}/>
+        <Route path="/friends" element={<Friends/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/settings" element={<Settings/>}/>
       </Route>
     </>
   )

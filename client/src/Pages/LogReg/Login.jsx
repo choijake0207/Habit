2c20a33a81ca38cc36417e6a287f714124ece61e
choijake0207 <jake.ch0712@gmail.com>
2025-01-24 18:08:38 -0500

    import form

---
+++ client/src/Pages/LogReg/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignForm from '../../Components/SignForms/SignForm'
+
+export default function Login() {
+  return (
+    <div class="page login-page">
+      <SignForm
+        type={"login"}
+      />
+    </div>
+  )
+}
---
+++ client/src/Pages/LogReg/Register.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SignForm from '../../Components/SignForms/SignForm'
+
+export default function Register() {
+  return (
+    <div class="page register-page">
+        <SignForm/>
+    </div>
+  )
+}

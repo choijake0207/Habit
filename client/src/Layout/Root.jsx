0346608b41865a119931c5e0372ac32d0d869a6d
choijake0207 <jake.ch0712@gmail.com>
2025-01-24 16:46:42 -0500

    add: basic skeletal layout

--- client/src/Layout/Root.jsx
+++ client/src/Layout/Root.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import {NavLink, Outlet} from "react-router"
+import {House, GearSix, Users, SquaresFour, ChartPie, User, SignOut} from "phosphor-react"
+import "./root.css"
 
 
 export default function Root() {
   return (
-    <div class="root-layout"> 
-        <aside>
-          <nav>
-            <NavLink to="/">Dashboard</NavLink>
-            <NavLink to="/myhabits">My Habits</NavLink>
-            <NavLink to="/friends">Friends</NavLink>
-            <NavLink to="/analytics">Analytics</NavLink>
-            <NavLink to="/profile">Profile</NavLink>
-            <NavLink to="/settings">Settings</NavLink>
-          </nav>
-        </aside>
+    <div class="root"> 
+
+        <nav class="root_navbar">
+          <div className="logo-wrap">Forwards</div>
+          <NavLink  to="/"><House/>Dashboard</NavLink>
+          <NavLink  to="/myhabits"><SquaresFour/>My Habits</NavLink>
+          <NavLink  to="/friends"><Users/>Friends</NavLink>
+          <NavLink  to="/analytics"><ChartPie/>Analytics</NavLink>
+          <NavLink  to="/profile"><User/>Profile</NavLink>
+          <NavLink  to="/settings"><GearSix/>Settings</NavLink>
+          <button class="navbar_signout"><SignOut/></button>
+        </nav>
 
         <main class="main-content">
           <Outlet/>

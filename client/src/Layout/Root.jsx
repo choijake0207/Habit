e09d52645c9c536244d81d26a331be2685766cfb
choijake0207 <jake.ch0712@gmail.com>
2025-01-24 15:28:40 -0500

    add all navlinks

--- client/src/Layout/Root.jsx
+++ client/src/Layout/Root.jsx
@@ -4,10 +4,21 @@ import {NavLink, Outlet} from "react-router"
 
 export default function Root() {
   return (
-    <div class="root-layout">   
-        <NavLink to="/">Dashboard</NavLink>
+    <div class="root-layout"> 
+        <aside>
+          <nav>
+            <NavLink to="/">Dashboard</NavLink>
+            <NavLink to="/myhabits">My Habits</NavLink>
+            <NavLink to="/friends">Friends</NavLink>
+            <NavLink to="/analytics">Analytics</NavLink>
+            <NavLink to="/profile">Profile</NavLink>
+            <NavLink to="/settings">Settings</NavLink>
+          </nav>
+        </aside>
 
-        <Outlet/>
+        <main class="main-content">
+          <Outlet/>
+        </main>
 
     </div>
   )
